b94863436c949f8f7bbf1eb48adf4190af7031cc
Jayden Tavares <jayden.tavares@hotmail.com>
2023-03-12 15:10:32 +1100

    add select tilemap, add character and turn controller, remake and add camera controller

--- src/controllers/CameraController.cs
+++ src/controllers/CameraController.cs
@@ -1,77 +1,61 @@
 using Godot;
-using System;
 
 public partial class CameraController : Controller
 {
     [Export]
-    private int moveSpeed = 10;
+    private int _moveSpeed = 400;
 
     [Export]
-    private int rotationSpeed = 3;
+    private float _minZoom = 0.25f;
 
     [Export]
-    private int minZoom = 5;
+    private float _maxZoom = 2f;
 
-    [Export]
-    private int maxZoom = 20;
-
-    private Camera3D _camera;
-    private Node3D _cameraContainer;
+    private Camera2D _camera;
 
     public override void Run()
     {
-        _cameraContainer = GetNode<Node3D>("/root/Main/World/CameraContainer");
-        _camera = _cameraContainer.GetNode<Camera3D>("Camera3D");
+        _camera = GetNode<Camera2D>("/root/Main/World/Camera2D");
     }
 
     public override void _Process(double delta)
     {
-        int rotateDirection = 0;
-        Vector3 movement = new Vector3();
-
-        if (Input.IsActionPressed("CameraRotateRight"))
-        {
-            rotateDirection -= 1;
-        }
-
-        if (Input.IsActionPressed("CameraRotateLeft"))
-        {
-            rotateDirection += 1;
-        }
+        Vector2 movement = new Vector2();
 
         if (Input.IsActionPressed("CameraMoveUp"))
         {
-            movement += Vector3.Forward;
+            movement += Vector2.Up;
         }
 
         if (Input.IsActionPressed("CameraMoveLeft"))
         {
-            movement += Vector3.Left;
+            movement += Vector2.Left;
         }
 
         if (Input.IsActionPressed("CameraMoveDown"))
         {
-            movement += Vector3.Back;
+            movement += Vector2.Down;
         }
 
         if (Input.IsActionPressed("CameraMoveRight"))
         {
-            movement += Vector3.Right;
+            movement += Vector2.Right;
         }
 
-        _cameraContainer.RotateY(rotateDirection * rotationSpeed * (float) delta);
-        _cameraContainer.Translate(movement * moveSpeed * (float) delta);
+        _camera.Translate(movement * _moveSpeed * (float) delta);
     }
 
     public override void _UnhandledInput(InputEvent @event)
     {
         if (@event.IsActionPressed("CameraZoomIn"))
         {
-            _camera.Size = Mathf.Max(minZoom, _camera.Size - 1);
+            float zoom = Mathf.Min(_maxZoom, _camera.Zoom.X + 0.5f);
+            _camera.Zoom = new Vector2(zoom, zoom);
         }
         else if (@event.IsActionPressed("CameraZoomOut"))
         {
-            _camera.Size = Mathf.Min(maxZoom, _camera.Size + 1);
+            float zoom = Mathf.Max(_minZoom, _camera.Zoom.X - 0.5f);
+            _camera.Zoom = new Vector2(zoom, zoom);
         }
     }
 }
